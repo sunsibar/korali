4b74792989083419bab3b9eeb5c1024d3a15fd38
wadaniel <wadaniel@ethz.ch>
2019-11-01 09:24:55 +0100

    added plotmatrixhist and fixed term crit

--- tutorials/a10-gm/x.py
+++ tutorials/a10-gm/x.py
@@ -51,7 +51,7 @@ for i in range(4):
 
 k["Solver"]["Type"]  = "Sampler/MCMC"
 k["Solver"]["Burn In"] = 500
-k["Solver"]["Termination Criteria"]["Max Chain Length"] = 50000
+k["Solver"]["Termination Criteria"]["Max Samples"] = 50000
 for i in range(4):
   k["Variables"][i]["Initial Mean"] = -1.0
   k["Variables"][i]["Initial Standard Deviation"] = 0.4
